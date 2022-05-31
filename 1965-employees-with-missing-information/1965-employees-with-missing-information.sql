6594db00998b64cc1756b6ccfb2ff4fda516d9fd
Ankit kumar Mishra <71301176+ankitmishra067@users.noreply.github.com>
2022-05-31 21:24:25 +0530

    Time: 616 ms (31.35%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,10 @@
+
+select employee_id from Employees
+where employee_id not in
+(select employee_id from salaries)
+UNION
+select employee_id from salaries
+where employee_id not in
+(select employee_id from employees)
+
+order by employee_id asc;
